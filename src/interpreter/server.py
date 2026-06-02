571fcc6de7a545a7c7623e6f329df9ee8514a71d
orangeyummy <orangetastegood2123@gmail.com>
2026-06-02 19:10:48 -0400

    fixed typo and added info page+routing

--- src/interpreter/server.py
+++ src/interpreter/server.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tariqgpt.duckdns.org"],
+    allow_origins=["http://localhost:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
---
+++ src/routes/info.svelte
@@ -0,0 +1,18 @@
+<script lang=ts>
+    import tariqPfp from "../assets/tariqgpt-logo.svg";
+
+</script>
+<h1>About</h1>
+<p>This page will include much information on tariq, describing his early days, such as his training and funny events</p>
+
+<h2>Tariq Prime</h2>
+<p>Tariq Prime, or Tariq0.1 was the very first released iteration of tariq, he had not a lot going for them but sure loved the war of 1812</p>
+
+<h2>Tariq 0.4 or "Red Tariq"</h2>
+<p>At this point Tariq has scrapped wikipedia, and came accross not very positive information</p>
+<p>Red Tariq was racist, sexist, and even claimed to be hitler (to be clear, through wikipedia and not our choice, no other version is as hateful as him), but in particular <b>HATES</b> Johnrei, seriously, just type johnrei or something about an annoying person and he will never end rambling about how terrible the guy is</p>
+<small>I almost feel bad for the guy.</small>
+
+<h2>Fat tariq</h2>
+<p>Tariq 0.6 by this point has had access to discord for a long time, he is the longest running version of Tariq, the second being 0.5.5. He linked us to an... odd twitter page, which I will not share.</p>
+<p>The reason 0.6 has been nicknamed by me fat tariq is because he is an issue as of now. He tripled and size and broke the optimizer for all of his checkpoints, requiring me or @poopypants (github) to have to fix it just him. Such greed.</p>
